f954c869e9ddd7af58a7610ca33909d4449c2ded
sofya <s_roganskaya@kbtu.kz>
2026-05-21 21:14:31 +0500

    докер путь изменен

--- initdb/02_load_data.sql
+++ initdb/02_load_data.sql
@@ -1,3 +1,3 @@
 copy transactions(sender_id, receiver_id, date, amount_kzt, description, doc_type)
-from '/docker-entrypoint-initdb.d/transactions_clean.csv'
+from '/data/transactions_clean.csv'
 with (FORMAT csv, HEADER true, ENCODING 'UTF8');
