3f54eddd8a34173a4dda7cd3ae9f1626699c1c69
sofya <s_roganskaya@kbtu.kz>
2026-05-21 21:39:11 +0500

    имя базы данных изменено

--- app/db.py
+++ app/db.py
@@ -9,7 +9,7 @@ def DBc():
     return psycopg2.connect(
         host=os.getenv("DB_HOST", "localhost"),
         port=os.getenv("DB_PORT", "5432"),
-        dbname=os.getenv("DB_NAME", "Transactions"),
+        dbname=os.getenv("DB_NAME", "transactions"),
         user=os.getenv("DB_USER", "postgres"),
         password=os.getenv("DB_PASSWORD", "postgres"),
         cursor_factory=RealDictCursor,
